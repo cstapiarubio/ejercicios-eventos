4fa07bc1ef3db333d4b712bfcc2760e15e2da41b
cstapiarubio <cstapiarubio@gmail.com>
2017-05-22 19:34:41 -0400

    estan desarrolladas las funciones de onmuoseover y out

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,13 @@ var impresion=document.getElementById("contenedor");
 mensaje.addEventListener("click",function(){
 	impresion.innerHTML="¡Hola Mundo!";
 })
+
+var elem =document.getElementById('hello');
+function pasar(elem){
+	alert("¡Estás sobre mí!");
+}
+	
+var elem2=document.getElementById("bye");
+function salir(elem2){
+alert("¡No estás sobre mí!");
+	}
