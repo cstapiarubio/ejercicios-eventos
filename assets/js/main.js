3ae3b5b657c7f13d6939132cef660329b5c748f0
cstapiarubio <cstapiarubio@gmail.com>
2017-05-22 18:59:38 -0400

    esta listo el boton y la impresion hola mundo, tambien algo de diseño

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
-main.js
+var mensaje=document.getElementById('demo');
+var impresion=document.getElementById("contenedor");
+
+mensaje.addEventListener("click",function(){
+impresion.innerHTML="Hola Mundo";
+})
